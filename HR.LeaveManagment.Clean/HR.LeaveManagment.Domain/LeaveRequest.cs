6bb965fe6117cbbed811ccbb4adc752e781ce654
Myroslav <muroslavkl@gmail.com>
2024-02-27 17:45:21 +0200

    Feat:Add entities to Core folder

---
+++ HR.LeaveManagment.Clean/HR.LeaveManagment.Domain/LeaveRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using HR.LeaveManagment.Domain.Common;
+
+namespace HR.LeaveManagment.Domain;
+
+public class LeaveRequest:BaseEntity
+{
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    
+    public DateTime DateRequested { get; set; }
+    public string? RequestComments { get; set; }
+    
+    public bool? Approved { get; set; }
+    public bool? Cancelled { get; set; }
+    public string RequestingEmployeeId { get; set; } = string.Empty;
+}
